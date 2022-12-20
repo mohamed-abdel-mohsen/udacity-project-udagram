62d70f17311b627826ce4bf661644619ff732226
unknown <moh.mohsen8074@gmail.com>
2022-12-20 22:07:07 +0200

    Fixing lint errors

--- udagram/udagram-frontend/src/app/feed/services/feed.provider.service.ts
+++ udagram/udagram-frontend/src/app/feed/services/feed.provider.service.ts
@@ -11,19 +11,16 @@ export class FeedProviderService {
   currentFeed$: BehaviorSubject<FeedItem[]> = new BehaviorSubject<FeedItem[]>([]);
 
   constructor(private api: ApiService) { }
-
   async getFeed(): Promise<BehaviorSubject<FeedItem[]>> {
     const req = await this.api.get('/feed');
     const items = <FeedItem[]> req.rows;
     this.currentFeed$.next(items);
     return Promise.resolve(this.currentFeed$);
   }
-
   async uploadFeedItem(caption: string, file: File): Promise<any> {
     const res = await this.api.upload('/feed', file, {caption: caption, url: file.name});
     const feed = [res, ...this.currentFeed$.value];
     this.currentFeed$.next(feed);
     return res;
   }
-
 }
