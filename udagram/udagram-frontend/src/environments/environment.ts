1d9913b63c0cc486719169eb553db0e6f169e669
unknown <moh.mohsen8074@gmail.com>
2023-01-05 13:52:15 +0200

    FIXING FRONT END

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:3000/api/v0'
+  apiHost: 'http://mohsen-udagram.s3-website-us-east-1.amazonaws.com'
 };
 
 /*
